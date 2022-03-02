bfceca107d6cbf522de1d806f8e77ecb9324b8b3
EnasHameed <alqaadienas@gmail.com>
2022-03-02 16:04:18 +0300

    chaing port to PORT in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.Port || 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.static('./assets'))
 app.set('view engine', 'ejs');
 
@@ -12,4 +12,4 @@ app.get('*', (req, res) => {
     res.send('notfound');
 });
 
-app.listen(port, () => console.log(`listening on port ${ port }`));
+app.listen(PORT, () => console.log(`listening on port ${ PORT }`));
